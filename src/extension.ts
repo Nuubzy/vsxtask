95c3ba730eccd97a1943dea92e833ee50e36837a
Todor Ivanov <todorivanov23.work@gmail.com>
2022-06-29 15:00:55 +0300

    Added Create new Kendo project

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const channel = vscode.window.createOutputChannel("Kendo Output");
+channel.show(true);
+channel.append("Starting Kendo project wizard");
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,10 +17,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsxtask.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('vsxtask.Welcome', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSXTask!');
+		vscode.window.showInformationMessage('Welcome from VSXTask!');
 	});
 	let secondcmd = vscode.commands.registerCommand('vsxtask.Pathing', () => {
 		// The code you place here will be executed every time your command is executed
@@ -37,10 +41,29 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showErrorMessage(message);
 		}
 	});
-		
+	let test = vscode.commands.registerCommand("markdown.test", () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		let message;
+		if (vscode.workspace.workspaceFolders !== undefined) {
+		  let f = vscode.workspace.workspaceFolders[0].uri.fsPath;
 	
+		//   message = `YOUR-EXTENSION: folder: ${f}`;
+	
+		//   vscode.window.showInformationMessage(message);
+		} else {
+		  message = "YOUR-EXTENSION: Working folder not found, open a folder an try again";
+	
+		  vscode.window.showErrorMessage(message);
+		}
+	  });
+	
+	
+
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(secondcmd);
+	context.subscriptions.push(test);
+	
 }
 
 // this method is called when your extension is deactivated
